5f136ab9a47c461bf1ae9ff876bd374f9006f66e
Chalupczak Jakub <chalujak@brightone.pl>
2018-06-21 12:02:48 +0200

    added git ignore

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        return "Hello World with Codeship!";
     }
 
     public static void main(String[] args) throws Exception {
