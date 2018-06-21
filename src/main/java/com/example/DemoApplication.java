76e83197c548780e76f24183e8476dbd9f7b1dbd
Chalupczak Jakub <chalujak@brightone.pl>
2018-06-21 11:36:36 +0200

    init commit

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@EnableAutoConfiguration
+public class DemoApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
